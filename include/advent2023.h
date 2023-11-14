6b6da1209a00aa7159750d0ed02df91324103435
Daryl Stark <daryl@dstark.nl>
2023-11-14 11:14:04 +0100

    Added the static web content

--- include/advent2023.h
+++ include/advent2023.h
@@ -2,20 +2,23 @@
 #define __ADVENT2023_H__
 
 #include <Arduino.h>
-#include <memory>
-#include <WiFiManager.h>
-#include <WebServer.h>
 #include <functional>
+#include <WiFiManager.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
 
 class Advent2023
 {
 
 private:
-    WebServer __web_server;
+    AsyncWebServer __web_server;
 
-    void __server_root_page();
+    void __server_root_page(AsyncWebServerRequest *request);
+    void __css_file(AsyncWebServerRequest *request);
+    void __js_file(AsyncWebServerRequest *request);
 
 public:
+    Advent2023();
     void setup();
     void loop();
 };
--- src/advent2023.cpp
+++ src/advent2023.cpp
@@ -1,5 +1,10 @@
 #include "advent2023.h"
 
+Advent2023::Advent2023()
+    : __web_server(80)
+{
+}
+
 void Advent2023::setup()
 {
     Serial.begin(115200);
@@ -12,20 +17,47 @@ void Advent2023::setup()
     if (res)
     {
         Serial.println("Setting up the app");
-        __web_server.on("/", std::bind(&Advent2023::__server_root_page, this));
+        __web_server.on(
+            "/",
+            std::bind(
+                &Advent2023::__server_root_page,
+                this,
+                std::placeholders::_1));
+        __web_server.on(
+            "/static/css/main.50ad4168.css",
+            std::bind(
+                &Advent2023::__css_file,
+                this,
+                std::placeholders::_1));
+        __web_server.on(
+            "/static/js/main.29523ef9.js",
+            std::bind(
+                &Advent2023::__js_file,
+                this,
+                std::placeholders::_1));
         __web_server.begin();
+        LittleFS.begin();
     }
 }
 
 void Advent2023::loop()
 {
-    __web_server.handleClient();
     delay(10);
 }
 
-void Advent2023::__server_root_page()
+void Advent2023::__server_root_page(AsyncWebServerRequest *request)
 {
-    __web_server.send(200, "text/html", "<p>Frontpage</p>");
+    request->send(LittleFS, "/index.html", "text/html");
+}
+
+void Advent2023::__css_file(AsyncWebServerRequest *request)
+{
+    request->send(LittleFS, "/static/css/main.50ad4168.css", "text/css");
+}
+
+void Advent2023::__js_file(AsyncWebServerRequest *request)
+{
+    request->send(LittleFS, "/static/js/main.29523ef9.js", "text/javascript");
 }
 
 /******************************************************************************/
